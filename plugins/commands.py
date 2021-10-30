bac87a8062b140a99651a335d6d71c01efd33389
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-10-30 23:31:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,13 +70,21 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url='http://t.me/EvaMariaBot?startgroup=true')
+
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
+
+            InlineKeyboardButton('🕵‍♂ Developer', url='https://t.me/mhd_thanzeer'),
+
+            InlineKeyboardButton('🎪 Group', url='https://t.me/wolfpackmedia')
+
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+
+            InlineKeyboardButton('🧯 Help ', callback_data='help'),
+
+            InlineKeyboardButton('📚 About', callback_data='about')
+
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
